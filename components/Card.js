935a9e616f5c2c1bc2fddcf0d779fde5e2c27d20
fikireab-m <fikireab.meku@gmail.com>
2024-02-03 21:33:38 +0300

    service card

---
+++ components/Card.js
@@ -0,0 +1,72 @@
+import React, { Children, useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const ROTATION_RANGE = 32.5;
+const HALF_ROTATION_RANGE = 32.5 / 2;
+
+const TiltCard = ({children}) => {
+  const ref = useRef(null);
+
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+
+  const handleMouseMove = (e) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+
+    setRotateX(rX);
+    setRotateY(rY);
+  };
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return;
+    setRotateX(0);
+    setRotateY(0);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transformStyle: "preserve-3d",
+        height:450,
+        width:300
+      }}
+      animate={{
+        rotateX,
+        rotateY,
+      }}
+      className="relative rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
+    >
+      <div
+        style={{
+          transform: "translateZ(75px)",
+          transformStyle: "preserve-3d",
+        }}
+        className="absolute inset-4 grid place-content-center rounded-xl bg-white shadow-lg"
+      >
+        <div
+          style={{
+            transform: "translateZ(50px)",
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default TiltCard;
